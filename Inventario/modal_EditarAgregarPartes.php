fd3958ecef4376065e4b827dd1c9cb273bdd8661
gabriel.gonzalez14@utp.ac.pa <gabriel.gonzalez14@utp.ac.pa>
2025-07-23 20:14:59 -0500

    Agregar Partes

--- Inventario/modal_EditarAgregarPartes.php
+++ Inventario/modal_EditarAgregarPartes.php
@@ -12,7 +12,7 @@
       </button>
     </div>
 
-    <form class="part-edit-modal__form" id="partForm">
+    <form class="part-edit-modal__form" id="partForm" method="POST" action="agregar_parte.php" enctype="multipart/form-data">
       <input type="hidden" id="partId">
 
       <div class="form-grid">
